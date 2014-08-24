6dbba7e9f547b7b16721631bab0f82987c4b1c31
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-08-25 01:29:53 +0300

    use figwheel template

--- project.clj
+++ project.clj
@@ -1,27 +1,38 @@
-(defproject watchlist "0.1.0"
+(defproject watchlist "0.1.0-SNAPSHOT"
   :description "Watchlist app"
   :url "http://watchlist-app.github.io"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2280"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.7.0"]
-                 [alandipert/storage-atom  "1.2.3"]
+                 [com.facebook/react "0.11.1"]
+                 [figwheel "0.1.4-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [cljs-http  "0.1.15"]
-                 [figwheel  "0.1.3-SNAPSHOT"]]
+                 [alandipert/storage-atom  "1.2.3"]
+                 [om "0.7.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
-            [lein-figwheel  "0.1.3-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-figwheel "0.1.4-SNAPSHOT"]]
 
   :source-paths ["src"]
 
-  :cljsbuild {:builds [{:id "dev"
+  :cljsbuild {
+    :builds [{:id "dev"
               :source-paths ["src"]
               :compiler {:output-to "resources/public/js/compiled/watchlist.js"
                          :output-dir "resources/public/js/compiled/out"
                          :optimizations :none
-                         :source-map true}}]}
-
-  :figwheel  {:http-server-root  "public"
-              :port 3449
-              :css-dirs  ["resources/public/css"]})
+                         :source-map true}}
+             {:id "min"
+              :source-paths ["src"]
+              :compiler {:output-to "www/watchlist.min.js"
+                         :optimizations :advanced
+                         :pretty-print false
+                         :preamble ["react/react.min.js"]
+                         :externs ["react/externs/react.js"]}}]}
+  :figwheel {
+             :http-server-root "public"
+             :server-port 3449
+             :css-dirs ["resources/public/css"]})
