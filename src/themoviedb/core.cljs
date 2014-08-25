f0cf4bff057f8533fb3d368ed3e3da61f5cf09cc
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-08-26 00:47:39 +0300

    method -> hash

--- src/themoviedb/core.cljs
+++ src/themoviedb/core.cljs
@@ -6,27 +6,23 @@
 
 (def ^:private api-key "")
 
-(defn img-size [kind size]
-  (case kind
-    :backdrop (case size
-                :small "w300"
-                :medium "w780"
-                :large "w1280"
-                :huge "original")
-    :poster (case size
-              :tiny "w92"
-              :very-small "w154"
-              :small "w185"
-              :ok "w342"
-              :medium "w500"
-              :large "w780"
-              :huge "original")))
+(def img-size {:backdrop {:small "w300"
+                          :medium "w780"
+                          :large "w1280"
+                          :huge "original" }
+               :poster   {:tiny "w92"
+                          :very-small "w154"
+                          :small "w185"
+                          :ok "w342"
+                          :medium "w500"
+                          :large "w780"
+                          :huge "original" } })
 
 
 
 
 (defn image-url [kind size url]
-  (str "http://image.tmdb.org/t/p/" (img-size kind size)  "/" url))
+  (str "http://image.tmdb.org/t/p/" (size (kind img-size))  "/" url))
 
 
 (defn- request [url query]
