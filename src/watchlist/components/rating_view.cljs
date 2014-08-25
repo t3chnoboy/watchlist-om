8b9f0ad6c0205eb3cbed6222fd61e76e30da7724
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-08-26 02:40:26 +0300

    replace deprecated 'replicate' with 'repeat'

--- src/watchlist/components/rating_view.cljs
+++ src/watchlist/components/rating_view.cljs
@@ -2,10 +2,10 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defn rating-view [{:keys [rating max-rating] :as data} owner]
+(defn rating-view [{:keys [rating max-rating]}]
   (om/component
     (apply dom/div #js {:className "rating"}
            (map #(dom/span nil %)
                 (concat
-                  (replicate (- max-rating rating) "☆")
-                  (replicate rating "★"))))))
+                  (repeat (- max-rating rating) "☆")
+                  (repeat rating "★"))))))
