3e8d8b5bc37f737dfaa492386f89c16faeda3341
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-09-24 23:23:04 +0300

    update deps

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
-                 [com.facebook/react "0.11.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/clojurescript "0.0-2342"]
+                 [com.facebook/react "0.11.2"]
                  [figwheel "0.1.4-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [cljs-http  "0.1.15"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cljs-http  "0.1.16"]
                  [alandipert/storage-atom  "1.2.3"]
-                 [om "0.7.1"]]
+                 [om "0.7.3"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-figwheel "0.1.4-SNAPSHOT"]]
